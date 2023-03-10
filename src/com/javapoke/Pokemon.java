615b499321736c92260840c432a448c0e8495f9f
Luisito Canlas <Luisito.Canlas@tlgcohort.com>
2023-03-10 15:56:08 -0800

    Pokemon initial update

--- src/com/javapoke/Pokemon.java
+++ src/com/javapoke/Pokemon.java
@@ -8,14 +8,14 @@ class Pokemon {
     int level;
     int hitPoints;
     boolean isFainted = false;
-    HashMap<String, Double> fight;
+    HashMap<String, Double> attack;
 
-
-    // constructor
-    public Pokemon(String name, int level, int hitPoints) {
+    // constructors
+    public Pokemon(String name, int level, int hitPoints, HashMap<String, Double> attack) {
         this.name = name;
         this.level = level;
         this.hitPoints = hitPoints;
+        this.attack = attack;
     }
 
     // accessors
@@ -23,5 +23,49 @@ class Pokemon {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public boolean isFainted() {
+        return isFainted;
+    }
+
+    public void setFainted(boolean fainted) {
+        isFainted = fainted;
+    }
+
+    public HashMap<String, Double> getAttack() {
+        return attack;
+    }
+
+    public void setAttack(HashMap<String, Double> attack) {  // put attack(s) and its value for the pokemon to use
+        this.attack = attack;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon{" +
+                "name='" + name + '\'' +
+                ", level=" + level +
+                ", hitPoints=" + hitPoints +
+                ", isFainted=" + isFainted +
+                '}';
+    }
 }
