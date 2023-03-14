cbf2ff5db10cb5e65d88644e8f81bb8f91e77aae
Luisito Canlas <Luisito.Canlas@tlgcohort.com>
2023-03-14 11:48:25 -0700

    Create Attack class

--- src/com/javapoke/Attack.java
+++ src/com/javapoke/Attack.java
@@ -1,8 +1,13 @@
 package com.javapoke;
 
+import java.util.Random;
+
 class Attack {
 
     public static int getDamage() {
-        return 0;
+        int pain = 0;
+        Random random = new Random();
+        pain = random.nextInt(30) + 15;      // damage will be between 15~45, attack modifier will be for future code
+        return pain;
     }
 }
