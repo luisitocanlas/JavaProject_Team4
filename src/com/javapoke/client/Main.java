18056ab46560d62c9f6d634c8208fd46f1001c84
Jorge Aponte <jorgealyaponte@yahoo.com>
2023-03-14 11:42:07 -0700

    add txt files for choosePokemon() and implement the txt files

--- src/com/javapoke/client/Main.java
+++ src/com/javapoke/client/Main.java
@@ -5,7 +5,7 @@ import com.javapoke.app.JavaPokeApp;
 class Main {
     public static void main(String[] args) {
         JavaPokeApp app = new JavaPokeApp();
-        // Uncomment Thursday night
+        // Uncomment Thursday night and make sure to run "run-splash.cmd" during presentation
 //        app.welcome("images/Poke.png", "images/credits.png");
         app.start();
     }
