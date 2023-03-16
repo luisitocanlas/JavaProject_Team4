1fead667a6696c7b8c4767717cc7eff77d7584f9
Jorge Aponte <jorgealyaponte@yahoo.com>
2023-03-16 11:21:03 -0700

    Create Dialogue class (Has-A Relationship with Trainer)

---
+++ src/com/javapoke/Dialogue.java
@@ -0,0 +1,100 @@
+package com.javapoke;
+
+import static com.apps.util.Console.*;
+
+class Dialogue {
+
+    public void loreleiDialogue() {
+        blankLines(1);
+        System.out.println("Welcome to Pokemon League!\n");
+        pause(2_000);
+        System.out.println("I am Lorelei of the Elite Four!\n");
+        pause(2_000);
+        System.out.println("No one can best me. Your Pokemon will be at my mercy!\n");
+        pause(2_000);
+        System.out.println("Hahaha! Are you ready?");
+        pause(5_000);
+        clear();
+    }
+
+    public void brunoDialogue() {
+        blankLines(1);
+        System.out.println("I am Bruno of the Elite Four!\n");
+        pause(2_000);
+        System.out.println("Through rigorous training, people and Pokemon can become stronger!\n");
+        pause(2_000);
+        System.out.println("I've weight trained with my Pokemon!\n");
+        pause(2_000);
+        System.out.println("We will grind you down with our superior power!\n");
+        pause(2_000);
+        System.out.println("Hoo hah!");
+        pause(5_000);
+        clear();
+    }
+
+    public void agathaDialogue() {
+        blankLines(1);
+        System.out.println("I am Agatha of the Elite Four!\n");
+        pause(2_000);
+        System.out.println("Jay's taken a lot of interest in you, child!\n");
+        pause(2_000);
+        System.out.println("That old duff was once tough and handsome! That was decades ago!\n");
+        pause(2_000);
+        System.out.println("Now he just wants to fiddle with his Effective Java!\n");
+        pause(2_000);
+        System.out.println("I will show you how a real trainer fights!");
+        pause(5_000);
+        clear();
+    }
+
+    public void lanceDialogue() {
+        blankLines(1);
+        System.out.println("Ah! I heard about you\n");
+        pause(2_000);
+        System.out.println("I lead the Elite Four! You can call me Lance the dragon trainer!\n");
+        pause(2_000);
+        System.out.println("You know that dragons are mythical Pokemon!\n");
+        pause(2_000);
+        System.out.println("They're hard to catch and raise, but their powers are superior!\n");
+        pause(2_000);
+        System.out.println("They're virtually indestructible!\n");
+        pause(2_000);
+        System.out.println("Well, are you ready to lose?\n");
+        pause(2_000);
+        System.out.println("Your challenge ends with me!");
+        pause(5_000);
+        clear();
+    }
+
+    public void joshDialogue() {
+        blankLines(1);
+        System.out.println("Hey! I was looking forward to seeing you!\n");
+        pause(2_000);
+        System.out.println("My rival should be strong to keep me sharp!\n");
+        pause(2_000);
+        System.out.println("While working on Effective Java, I looked all over for powerful Pokemon!\n");
+        pause(2_000);
+        System.out.println("Not only that, I managed to attain the strongest Pokemon known to man!\n");
+        pause(2_000);
+        System.out.println("And now! I'm the Pokemon League Champion\n");
+        pause(2_000);
+        System.out.println("Do you know what that means. I'll tell you!\n");
+        pause(2_000);
+        System.out.println("I'm the greatest Pokemon Trainer and also Java developer! \nFight me if you dare!");
+        pause(5_000);
+        clear();
+    }
+
+    public void surpriseDialogue() {
+        blankLines(1);
+        System.out.println("But suddenly you hear a voice...\n");
+        pause(2_000);
+        System.out.println("What's this?\n");
+        pause(2_000);
+        System.out.println("A real challenge?\n");
+        pause(2_000);
+        System.out.println("The current champion enters the stage\n");
+        pause(5_000);
+        clear();
+    }
+}
