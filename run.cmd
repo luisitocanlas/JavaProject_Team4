95d5bfa9b67255db6ee2a6a1741c0c2cd545185b
Luisito Canlas <Luisito.Canlas@tlgcohort.com>
2023-03-16 12:20:20 -0700

    updated run.cmd

--- run.cmd
+++ run.cmd
@@ -1,3 +1,9 @@
 @echo off
 cls
-java -classpath out\production\JavaProject_Team4;"lib\*" com.javapoke.client.Main
+rem   In this example, any third-party library JARs are located in 'lib' and added to the classpath [-classpath option].
+rem   Remove this "lib\*" entry from the classpath if you're not using any, which results in the following:
+rem   java -classpath javapoke-1.0.jar com.javapoke.client.Main
+
+rem   Note that your application JAR stays on the classpath, regardless of whether you're using additional libraries or not.
+
+java -classpath out\production\JavaProject_Team4;"lib\*" -splash:images/java.png com.javapoke.client.Main
