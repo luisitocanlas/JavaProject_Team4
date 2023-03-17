fc0203fcacd60768767107e2d070df78f0fff463
Luisito Canlas <Luisito.Canlas@tlgcohort.com>
2023-03-17 12:22:04 -0700

    Update item quantity

--- src/com/javapoke/app/PokeBattle.java
+++ src/com/javapoke/app/PokeBattle.java
@@ -32,8 +32,8 @@ class PokeBattle {
     private static String congratulationsBanner;
     private static String continuePrompt;
 
-    private int potion = 10;
-    private int superPotion = 5;
+    private int potion = 25;        // adjust for difficulty
+    private int superPotion = 15;   // adjust for difficulty
     private int maxHP;
     private boolean inBattle = false;
     private Pokemon activePokemon;
